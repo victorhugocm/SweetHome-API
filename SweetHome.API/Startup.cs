4796c766208929a9ebf3f541342fccc6ba74ad16
Victor Hugo <victorhugocm@outlook.com.br>
2020-07-29 20:29:03 -0300

    Criação da string de conexão no appsettings.json e adiciona o contexto do banco aos serviços do arquivo startup.cs

--- SweetHome.API/Startup.cs
+++ SweetHome.API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SweetHome.API.Models;
 
 namespace SweetHome.API
 {
@@ -25,6 +27,10 @@ namespace SweetHome.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adiciona o contexto do banco utilizado aos serviços, define o provedor e a string de conexão com o banco.
+            services.AddDbContext<SweetHomeContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
         }
 
