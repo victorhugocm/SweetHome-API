0b9e2126f65e8e6315d9ae5306e735fd4247ac37
Victor Hugo <victorhugocm@outlook.com.br>
2020-08-21 18:23:37 -0300

    Configurando CORS na startup.cs

--- SweetHome.API/Startup.cs
+++ SweetHome.API/Startup.cs
@@ -46,6 +46,13 @@ namespace SweetHome.API
 
             app.UseRouting();
 
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
